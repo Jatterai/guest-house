3ad67ceb17ca6d2c0bbb0327f16503463fb65ab1
Maiorova Yuna <maiorova.yuna@gmail.com>
2023-04-06 12:32:02 +0300

    feat: add two sections

--- js/slider.js
+++ js/slider.js
@@ -2,4 +2,8 @@ const slider_container = document.querySelector('.reviews__row');
 const left_button = document.getElementById('left-button');
 const right_button = document.getElementById('right-button');
 
+function thenItemDisapper(e) {
+
+}
+
 Array.from(slider_container.children).forEach(e => console.log(e));
